febc04a0030898198d883cb7508697c1f5841521
Catherine Higgins <66173666+CatherineH1@users.noreply.github.com>
2025-01-27 13:06:10 +0000

    Add files via upload

--- simulations.R
+++ simulations.R
@@ -11,23 +11,35 @@ library(cluster)
 data = read.csv("Section 3.1 Equally Sized Modules.csv")
 id = as.matrix(read.csv("groundtruth1.csv", header = F))
 
-stihc = stIHC(data)
-ARI = adjustedRandIndex(stihc$label,id) #Compute Adjusted Rand Index
+stihc = stIHC(data) #Run stIHC
 
+ARI = adjustedRandIndex(stihc$label,id) #Compute Adjusted Rand Index
 
 ##################################################################################################################################################################
 # Simulation from Section 3.2: Imbalanced Modules
 data = read.csv("Section 3.2 Imbalanced Modules.csv")
 id = as.matrix(read.csv("groundtruth23.csv", header = F))
 
-stihc = stIHC(data)
+stihc = stIHC(data) #Run stIHC
+
 ARI = adjustedRandIndex(stihc$label,id) #Compute Adjusted Rand Index
+
+data <- t(data)
+data <- data[-c(1, 2), ]
+DBI =as.numeric(intCriteria(data, as.integer(stihc$label), c("Davies_Bouldin"))) #Compute Davies-Bouldin Index
+
 
 ##################################################################################################################################################################
 # Simulation from Section 3.3: Sparse and Imbalanced Modules
 data = read.csv("Section 3.3 Sparse and Imbalanced Modules.csv")
 id = as.matrix(read.csv("groundtruth23.csv", header = F))
 
-stihc = stIHC(data)
+stihc = stIHC(data) #Run stIHC
+
 ARI = adjustedRandIndex(stihc$label,id) #Compute Adjusted Rand Index
+
+data <- t(data)
+data <- data[-c(1, 2), ]
+DBI =as.numeric(intCriteria(data, as.integer(stihc$label), c("Davies_Bouldin"))) #Compute Davies-Bouldin Index
+
 
